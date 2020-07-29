91331f97715882ad0c66e54f5e5ba7943023ed79
stuts <stu.j.franks@gmail.com>
2020-07-29 16:03:10 +0100

    Example config file

---
+++ config/.example.sh
@@ -0,0 +1,9 @@
+# AWS details
+export AWS_ACCESS_KEY_ID="MyAccessKey"
+export AWS_SECRET_ACCESS_KEY="MyAccessSecretKey"
+export REGION="eu-west-2"
+
+# Cluster name for messages
+CLUSTER="mycluster"
+# Slack Channel name
+CHANNEL="clusterchannel"
